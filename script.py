ac785cb93bb23832ad494b82a56038ddc05c6159
Renan Santos <renansantosmendes@gmail.com>
2021-07-11 13:28:40 -0300

    added docstring in functions

--- script.py
+++ script.py
@@ -2,13 +2,25 @@ import time
 import pyautogui
 
 
-def change_volume(time_interval) -> None:
+def change_volume(time_interval: int) -> None:
+    '''
+    Increase and decrease the system volume after a given time interval
+    
+    Parameter:
+    - time_interval (int): integer value representing time between volume up and down
+    '''
     pyautogui.press('volumedown')
     time.sleep(time_interval)
     pyautogui.press('volumeup')
     time.sleep(time_interval)
 
-def avoid_screen_lock(time_interval) -> None:
+def avoid_screen_lock(time_interval: int) -> None:
+    '''
+    Avoids screen lock executing a change in volume after a given time interval
+    
+    Parameter:
+    - time_interval (int): integer value representing time between volume up and down
+    '''
     while True:
         change_volume(time_interval)
 
