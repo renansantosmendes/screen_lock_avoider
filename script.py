841146872cfdbcad9edf80ea268a67e3284f222c
Renan Santos <renansantosmendes@gmail.com>
2021-07-11 13:23:21 -0300

    refactored script

--- script.py
+++ script.py
@@ -1,9 +1,20 @@
 import time
 import pyautogui
 
-while True:
+
+def change_volume(time_interval) -> None:
     pyautogui.press('volumedown')
-    time.sleep(10)
+    time.sleep(time_interval)
     pyautogui.press('volumeup')
-    time.sleep(10)
+    time.sleep(time_interval)
+
+def avoid_screen_lock(time_interval) -> None:
+    while True:
+        change_volume(time_interval)
+
+time_interval = 10
+
+if __name__ == '__main__':
+    print('Avoid lock screen script started...')
+    avoid_screen_lock(time_interval)
     
